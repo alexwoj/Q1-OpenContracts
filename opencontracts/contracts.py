637621da1936e9b777a99ca2e5ee04c00b657a87
alex.wojtowicz@gmail.com <git@warpnet.com.br>
2022-10-19 17:18:33 -0300

    added _check_for_errors method

--- opencontracts/contracts.py
+++ opencontracts/contracts.py
@@ -15,7 +15,10 @@ class Contracts:
     def get_top_N_open_contracts(self, open_contracts, renegotiated_contracts, top_n):
         """Returns a list of top-n Associate Ids ordered by amount owed to the institution.
 
-        This method will first remove all renegotiated contracts and will create a new list called
+        Before this method runs, it will call a pre-processor that will check for basic errors from user input.
+        (self._check_for_errors)
+
+        After that, this method will remove all renegotiated contracts and will create a new list called
         open_contracts_list.
 
         This list will then be ordered by debt value, from greatest to lower values.
@@ -25,6 +28,7 @@ class Contracts:
         This method will return the results (Associate Ids) as an ordered list by debt owed to the institution.
         """
 
+        self._check_for_errors(open_contracts, renegotiated_contracts, top_n)
         open_contracts_list = self._remove_renegotiated_contracts(open_contracts, renegotiated_contracts)
         ordered_open_contracts = self._order_open_contract_list(open_contracts_list)
         ordered_open_contracts_ranking = self._create_open_contracts_ranking(ordered_open_contracts, top_n)
@@ -54,4 +58,24 @@ class Contracts:
             ordered_open_contracts_ranking.append(associate_id)
 
         return ordered_open_contracts_ranking
+
+    def _check_for_errors(self, open_contracts, renegotiated_contracts, top_n):
+        """Performs basic checking on user input"""
+
+        number_of_open_contracts = self._get_number_of_open_contracts(open_contracts, renegotiated_contracts)
+        self._check_max_top_n(number_of_open_contracts, top_n)
+
+    def _check_max_top_n(self, number_of_open_contracts, top_n):
+        """Checks if top_n is greater than the number of open contracts.
+
+        Open contracts formula: number of instantiated contracts - renegotiated contracts
+        """
+        if top_n > number_of_open_contracts:
+            print('[ERROR] top_n value of ', top_n, 'cannot be greater that number of ', number_of_open_contracts,
+                  'open contracts')
+
+    def _get_number_of_open_contracts(self, open_contracts, renegotiated_contracts):
+        """Returns the number of open contracts, removing renegotiated contracts"""
+
+        return len(open_contracts) - len(renegotiated_contracts)
 
